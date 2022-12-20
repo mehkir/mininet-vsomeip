8e719af1dcb44b284ac04e13a22d9ac274571e55
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2022-12-20 10:15:29 +0100

    Implement request cache

--- implementation/service_authentication/include/request_cache.hpp
+++ implementation/service_authentication/include/request_cache.hpp
@@ -6,8 +6,6 @@
 #include <boost/asio/ip/address_v4.hpp>
 #include "vsomeip/primitive_types.hpp"
 
-#define IP_ADDRESS  boost::asio::ip::address_v4
-
 struct challenger_data {
     int random_nonce;
     unsigned char* certificate;
@@ -16,10 +14,16 @@ struct challenger_data {
 class request_cache
 {
 private:
-    std::map<std::tuple<IP_ADDRESS, std::tuple<vsomeip_v3::service_t, vsomeip_v3::instance_t>>, challenger_data> request_cache_map;
+    std::map<std::tuple<boost::asio::ip::address_v4, vsomeip_v3::service_t, vsomeip_v3::instance_t>, challenger_data> request_map;
 public:
     request_cache(/* args */);
     ~request_cache();
+    void addRequest(boost::asio::ip::address_v4 ipAddress, vsomeip_v3::service_t serviceId,
+                    vsomeip_v3::instance_t instanceId, challenger_data challengerData);
+    challenger_data getRequest(boost::asio::ip::address_v4 ipAddress, vsomeip_v3::service_t serviceId,
+                    vsomeip_v3::instance_t instanceId);
+    void removeRequest(boost::asio::ip::address_v4 ipAddress, vsomeip_v3::service_t serviceId,
+                    vsomeip_v3::instance_t instanceId);
 };
 
 #endif /* VSOMEIP_REQUEST_CACHE_HPP */
--- implementation/service_authentication/src/request_cache.cpp
+++ implementation/service_authentication/src/request_cache.cpp
@@ -1,9 +1,24 @@
 #include "../include/request_cache.hpp"
 
-request_cache::request_cache(/* args */)
+request_cache::request_cache()
 {
 }
 
 request_cache::~request_cache()
 {
+}
+
+void request_cache::addRequest(boost::asio::ip::address_v4 ipAddress, vsomeip_v3::service_t serviceId,
+                               vsomeip_v3::instance_t instanceId, challenger_data challengerData) {
+    request_map[{ipAddress, serviceId, instanceId}] = challengerData;
+}
+
+challenger_data request_cache::getRequest(boost::asio::ip::address_v4 ipAddress, vsomeip_v3::service_t serviceId,
+                    vsomeip_v3::instance_t instanceId) {
+    return request_map[{ipAddress, serviceId, instanceId}];
+}
+
+void request_cache::removeRequest(boost::asio::ip::address_v4 ipAddress, vsomeip_v3::service_t serviceId,
+                vsomeip_v3::instance_t instanceId) {
+    request_map.erase({ipAddress, serviceId, instanceId});
 }
