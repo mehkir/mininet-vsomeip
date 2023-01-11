73897e6c69e0d7c6ace7d32c45ae5cbc3bfb7f58
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-01-11 20:39:15 +0100

    Simplify notify automization

--- eval_automization_scripts/start_notify_app.bash
+++ eval_automization_scripts/start_notify_app.bash
@@ -1,18 +1,6 @@
 #!/bin/bash
-MAX_RUNS=20
 
-printUsage() {
-    printf "Usage $0 <first_run_number>\n"
-    printf "Example: $0 0\n"
-}
-
-if [[ $# -gt 0 ]]; then
-    if [[ $1 -lt $((MAX_RUNS-1)) ]]; then
-        cd /home/vsomeip/
-        /usr/bin/cmake --build /home/vsomeip/build --config Debug --target all --
-        /usr/bin/cmake --build /home/vsomeip/build --config Debug --target examples --
-        env VSOMEIP_CONFIGURATION=/home/vsomeip/config/vsomeip-udp-service.json VSOMEIP_APPLICATION_NAME=service-sample /home/vsomeip/build/examples/notify-sample 1>/dev/null &
-    fi
-else
-    printUsage
-fi
+cd /home/vsomeip/
+/usr/bin/cmake --build /home/vsomeip/build --config Debug --target all --
+/usr/bin/cmake --build /home/vsomeip/build --config Debug --target examples --
+env VSOMEIP_CONFIGURATION=/home/vsomeip/config/vsomeip-udp-service.json VSOMEIP_APPLICATION_NAME=service-sample /home/vsomeip/build/examples/notify-sample 1>/dev/null &
