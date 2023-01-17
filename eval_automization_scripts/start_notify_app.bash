25272e3ded0b0a9129aa479b2142f90c16bf1852
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-01-17 18:33:13 +0100

    Separate compilation and start of apps in scripts

--- eval_automization_scripts/start_notify_app.bash
+++ eval_automization_scripts/start_notify_app.bash
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-cd /home/vsomeip/
-/usr/bin/cmake --build /home/vsomeip/build --config Debug --target all -- 1>/dev/null
-/usr/bin/cmake --build /home/vsomeip/build --config Debug --target examples -- 1>/dev/null
-env VSOMEIP_CONFIGURATION=/home/vsomeip/config/vsomeip-udp-service.json VSOMEIP_APPLICATION_NAME=service-sample /home/vsomeip/build/examples/notify-sample 1>/dev/null &
+compile() {
+    printf "Compiling notifiy-app..."
+    cd /home/vsomeip/
+    /usr/bin/cmake --build /home/vsomeip/build --config Debug --target all -- 1>/dev/null
+    /usr/bin/cmake --build /home/vsomeip/build --config Debug --target examples -- 1>/dev/null
+    printf " done.\n"
+}
+
+start() {
+    cd /home/vsomeip/
+    env VSOMEIP_CONFIGURATION=/home/vsomeip/config/vsomeip-udp-service.json VSOMEIP_APPLICATION_NAME=service-sample /home/vsomeip/build/examples/notify-sample 1>/dev/null &
+}
+
+if [ $# -gt 0 ]; then
+    case $1 in
+        "compile")
+            compile
+            ;;
+        "start")
+            start
+            ;;
+        *)
+            echo "Not enough arguments." 1>&2
+            ;;
+    esac
+else
+    echo "Not enough arguments." 1>&2
+fi
--- eval_automization_scripts/start_subscribe_app.bash
+++ eval_automization_scripts/start_subscribe_app.bash
@@ -3,10 +3,16 @@ RUN_NUM=0
 MAX_RUNS=20
 FILE_NUM=0
 
-start_app() {
+compile() {
+    printf "Compiling subscribe-app..."
     cd /home/vsomeip/
     /usr/bin/cmake --build /home/vsomeip/build --config Debug --target all -- 1>/dev/null
     /usr/bin/cmake --build /home/vsomeip/build --config Debug --target examples -- 1>/dev/null
+    printf " done.\n"
+}
+
+start() {
+    cd /home/vsomeip/
     env VSOMEIP_CONFIGURATION=/home/vsomeip/config/vsomeip-udp-client.json VSOMEIP_APPLICATION_NAME=client-sample /home/vsomeip/build/examples/subscribe-sample 1>/dev/null &
     while [ -z $(pgrep subscribe-sampl) ]; do
         sleep 1
@@ -16,26 +22,28 @@ start_app() {
 function on_exit() {
     echo "Terminating processes..."
     pkill subscribe-sampl
-    ssh vsomeip_server "pkill start_noti; pkill notify-samp;"
+    ssh vsomeip_server "pkill notify-samp; pkill start_noti;"
     echo "Terminated processes."
     exit 1
 }
 
 trap 'on_exit' SIGINT
 
-ssh vsomeip_server "pkill start_noti; pkill notify-samp;"
+ssh vsomeip_server "pkill notify-samp; pkill start_noti;"
 pkill subscribe-sampl
+ssh vsomeip_server "/home/vsomeip/eval_automization_scripts/start_notify_app.bash compile" &
+compile
 while [[ $RUN_NUM -lt $MAX_RUNS ]]; do
     while [ -f "/home/vsomeip/timestamp_results/timepoints-#${FILE_NUM}.csv" ]; do
         FILE_NUM=$((FILE_NUM+1))
     done
-    ssh vsomeip_server "/home/vsomeip/eval_automization_scripts/start_notify_app.bash" 1>/dev/null &
+    ssh vsomeip_server "/home/vsomeip/eval_automization_scripts/start_notify_app.bash start" 1>/dev/null &
     while [ -z $(ssh vsomeip_server "pgrep notify-samp") ]; do
         sleep 1
     done
     #echo "vsomeip server is started"
     sleep 5 # give server some time to initialize (maybe choose a smaller value)
-    start_app
+    start
     #echo "vsomeip client is started"
     while [ ! -f "/home/vsomeip/timestamp_results/timepoints-#${FILE_NUM}.csv" ]; do
         sleep 1
@@ -48,5 +56,5 @@ while [[ $RUN_NUM -lt $MAX_RUNS ]]; do
     echo "RUN $RUN_NUM finished."
     RUN_NUM=$((RUN_NUM+1))
     pkill subscribe-sampl
-    ssh vsomeip_server "pkill start_noti; pkill notify-samp;"
+    ssh vsomeip_server "pkill notify-samp; pkill start_noti;"
 done
