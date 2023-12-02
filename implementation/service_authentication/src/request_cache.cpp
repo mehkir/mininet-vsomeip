5f85b622372fcbf46cd5606fb684dfe524813223
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-12-02 06:05:52 +0100

    Refactor lock guard name

--- implementation/service_authentication/src/request_cache.cpp
+++ implementation/service_authentication/src/request_cache.cpp
@@ -12,7 +12,7 @@ namespace vsomeip_v3 {
     }
 
     request_cache* request_cache::get_instance() {
-        std::lock_guard<std::mutex> lockguard(mutex_);
+        std::lock_guard<std::mutex> guard(mutex_);
         if(instance_ == nullptr) {
             instance_ = new request_cache();
         }
@@ -21,14 +21,14 @@ namespace vsomeip_v3 {
 
     void request_cache::add_request_nonce(boost::asio::ip::address_v4 _ipv4_address, vsomeip_v3::service_t _service,
                                 vsomeip_v3::instance_t _instance, std::vector<unsigned char> _nonce) {
-        std::lock_guard<std::mutex> lockGuard(mutex_);
+        std::lock_guard<std::mutex> guard(mutex_);
         auto key_tuple = make_key_tupe(_ipv4_address, _service, _instance);
         request_map_[key_tuple].nonce_ = _nonce;
     }
 
     bool request_cache::has_nonce_and_remove(boost::asio::ip::address_v4 _ipv4_address, vsomeip_v3::service_t _service,
                         vsomeip_v3::instance_t _instance, std::vector<unsigned char> _nonce) {
-        std::lock_guard<std::mutex> lockGuard(mutex_);
+        std::lock_guard<std::mutex> guard(mutex_);
         auto key_tuple = make_key_tupe(_ipv4_address, _service, _instance);
         bool has_nonce = request_map_.count(key_tuple) && !request_map_[key_tuple].nonce_.empty() && request_map_[key_tuple].nonce_ == _nonce;
         if (has_nonce) { request_map_[key_tuple].nonce_.clear(); }
@@ -37,7 +37,7 @@ namespace vsomeip_v3 {
 
     void request_cache::add_request_certificate(boost::asio::ip::address_v4 _ipv4_address, vsomeip_v3::service_t _service,
                         vsomeip_v3::instance_t _instance, std::vector<unsigned char> _certificate_data) {
-        std::lock_guard<std::mutex> lockGuard(mutex_);
+        std::lock_guard<std::mutex> guard(mutex_);
         auto key_tuple = make_key_tupe(_ipv4_address, _service, _instance);
         request_map_[key_tuple].certificate_data_ = _certificate_data;
     }
