474a96ccb9930622b052b8674f5d65ed3dbc3fa8
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-12-06 21:45:07 +0100

    Add eventgroup subscription cache class

---
+++ implementation/service_authentication/include/eventgroup_subscription_cache.hpp
@@ -0,0 +1,67 @@
+#ifndef VSOMEIP_V3_EVENTGROUP_SUBSCRIPTION_CACHE_HPP
+#define VSOMEIP_V3_EVENTGROUP_SUBSCRIPTION_CACHE_HPP
+
+#include <mutex>
+#include <vsomeip/primitive_types.hpp>
+#include "../../routing/include/eventgroupinfo.hpp"
+#include "../../service_discovery/include/remote_subscription_ack.hpp"
+#include <boost/asio/ip/address_v4.hpp>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+
+namespace vsomeip_v3 {
+    struct eventgroup_subscription_cache_entry {
+        public:
+            client_t client_ = -1;
+            service_t service_ = -1;
+            instance_t instance_ = -1;
+            eventgroup_t eventgroup_ = -1;
+            major_version_t major_ = -1;
+            ttl_t ttl_ = -1;
+            uint8_t counter_ = -1;
+            uint16_t reserved_ = -1;
+            boost::asio::ip::address_v4 first_address_ = boost::asio::ip::address_v4::from_string("0.0.0.0");
+            uint16_t first_port_ = -1;
+            bool is_first_reliable_ = false;
+            boost::asio::ip::address_v4 second_address_ = boost::asio::ip::address_v4::from_string("0.0.0.0");
+            uint16_t second_port_ = -1;
+            bool is_second_reliable_ = false;
+            std::shared_ptr<sd::remote_subscription_ack> acknowledgement_;
+            bool is_stop_subscribe_subscribe_ = false;
+            bool force_initial_events_ = false;
+            std::set<client_t> clients_;
+            std::set<std::pair<bool, std::uint16_t>> expired_ports_;
+            bool sd_acceptance_required_;
+            bool accept_entries_;         
+            std::shared_ptr<eventgroupinfo> info_;
+            std::vector<unsigned char> nonce_;
+            std::vector<byte_t> signature_;
+    };
+
+    class eventgroup_subscription_cache {
+    private:
+        eventgroup_subscription_cache();
+        ~eventgroup_subscription_cache();
+        static std::mutex mutex_;
+        static eventgroup_subscription_cache* instance_;
+        std::map<std::tuple<client_t, service_t, instance_t, major_version_t>, eventgroup_subscription_cache_entry> eventgroup_subscription_cache_map_;
+        std::tuple<client_t, service_t, instance_t, major_version_t> make_key_tuple(client_t _client, service_t _service, instance_t _instance, major_version_t _major);
+    public:
+        static eventgroup_subscription_cache* get_instance();
+
+        eventgroup_subscription_cache(eventgroup_subscription_cache const&) = delete;
+        eventgroup_subscription_cache(eventgroup_subscription_cache&&) = delete;
+        eventgroup_subscription_cache& operator=(eventgroup_subscription_cache &) = delete;
+        eventgroup_subscription_cache& operator=(eventgroup_subscription_cache &&) = delete;
+
+        void add_eventgroup_subscription_cache_entry(client_t _client, service_t _service, instance_t _instance, eventgroup_t _eventgroup, major_version_t _major, ttl_t _ttl, uint8_t _counter, uint16_t _reserved, const boost::asio::ip::address_v4 _first_address, uint16_t _first_port, bool _is_first_reliable,
+                                const boost::asio::ip::address_v4 _second_address, uint16_t _second_port, bool _is_second_reliable, std::shared_ptr<sd::remote_subscription_ack> _acknowledgement, bool _is_stop_subscribe_subscribe, bool _force_initial_events, std::set<client_t> _clients,
+                                std::set<std::pair<bool, std::uint16_t>> _expired_ports, bool _sd_acceptance_required, bool _accept_entries, std::shared_ptr<eventgroupinfo> _info, std::vector<unsigned char> _nonce, std::vector<byte_t> _signature);
+        void remove_eventgroup_subscription_cache_entry(client_t _client, service_t _service, instance_t _instance, major_version_t _major);
+        eventgroup_subscription_cache_entry get_eventgroup_subscription_cache_entry(client_t client, service_t _service, instance_t _instance, major_version_t _major);
+    };
+    
+} /* end namespace vsomeip_v3 */
+#endif /* VSOMEIP_V3_EVENTGROUP_SUBSCRIPTION_CACHE_HPP */
---
+++ implementation/service_authentication/src/eventgroup_subscription_cache.cpp
@@ -0,0 +1,67 @@
+#include "../include/eventgroup_subscription_cache.hpp"
+
+namespace vsomeip_v3 {
+    std::mutex eventgroup_subscription_cache::mutex_;
+    eventgroup_subscription_cache* eventgroup_subscription_cache::instance_;
+
+    eventgroup_subscription_cache::eventgroup_subscription_cache() {
+    }
+    
+    eventgroup_subscription_cache::~eventgroup_subscription_cache() {
+    }
+
+    eventgroup_subscription_cache* eventgroup_subscription_cache::get_instance() {
+        std::lock_guard<std::mutex> lockguard(mutex_);
+        if(instance_ == nullptr) {
+            instance_ = new eventgroup_subscription_cache();
+        }
+        return instance_;
+    }
+
+    void eventgroup_subscription_cache::add_eventgroup_subscription_cache_entry(client_t _client, service_t _service, instance_t _instance, eventgroup_t _eventgroup, major_version_t _major, ttl_t _ttl, uint8_t _counter, uint16_t _reserved, const boost::asio::ip::address_v4 _first_address, uint16_t _first_port, bool _is_first_reliable,
+                                const boost::asio::ip::address_v4 _second_address, uint16_t _second_port, bool _is_second_reliable, std::shared_ptr<sd::remote_subscription_ack> _acknowledgement, bool _is_stop_subscribe_subscribe, bool _force_initial_events, std::set<client_t> _clients,
+                                std::set<std::pair<bool, std::uint16_t>> _expired_ports, bool _sd_acceptance_required, bool _accept_entries, std::shared_ptr<eventgroupinfo> _info, std::vector<unsigned char> _nonce, std::vector<byte_t> _signature) {
+        auto key_tuple = make_key_tuple(_client, _service, _instance, _major);
+        eventgroup_subscription_cache_entry& entry = eventgroup_subscription_cache_map_[key_tuple];
+        entry.client_ = _client;
+        entry.service_ = _service;
+        entry.instance_ = _instance;
+        entry.eventgroup_ = _eventgroup;
+        entry.major_ = _major;
+        entry.ttl_ = _ttl;
+        entry.counter_ = _counter;
+        entry.reserved_ = _reserved;
+        entry.first_address_ = _first_address;
+        entry.first_port_ = _first_port;
+        entry.is_first_reliable_ = _is_first_reliable;
+        entry.second_address_ = _second_address;
+        entry.second_port_ = _second_port;
+        entry.is_second_reliable_ = _is_second_reliable;
+        entry.acknowledgement_ = _acknowledgement;
+        entry.is_stop_subscribe_subscribe_ = _is_stop_subscribe_subscribe;
+        entry.force_initial_events_ = _force_initial_events;
+        entry.clients_ = _clients;
+        entry.expired_ports_ = _expired_ports;
+        entry.sd_acceptance_required_ = _sd_acceptance_required;
+        entry.accept_entries_ = _accept_entries;
+        entry.info_ = _info;
+        entry.nonce_ = _nonce;
+        entry.signature_ = _signature;
+    }
+
+    void eventgroup_subscription_cache::remove_eventgroup_subscription_cache_entry(client_t _client, service_t _service, instance_t _instance, major_version_t _major) {
+        std::lock_guard<std::mutex> lockguard(mutex_);
+        auto key_tuple = make_key_tuple(_client, _service, _instance, _major);
+        eventgroup_subscription_cache_map_.erase(key_tuple);
+    }
+
+    eventgroup_subscription_cache_entry eventgroup_subscription_cache::get_eventgroup_subscription_cache_entry(client_t _client, service_t _service, instance_t _instance, major_version_t _major) {
+        std::lock_guard<std::mutex> lockguard(mutex_);
+        auto key_tuple = make_key_tuple(_client, _service, _instance, _major);
+        return eventgroup_subscription_cache_map_[key_tuple];
+    }
+
+    std::tuple<client_t, service_t, instance_t, major_version_t> eventgroup_subscription_cache::make_key_tuple(client_t _client, service_t _service, instance_t _instance, major_version_t _major) {
+        return std::make_tuple(_client, _service, _instance, _major);
+    }
+} /* end namespace vsomeip_v3 */
