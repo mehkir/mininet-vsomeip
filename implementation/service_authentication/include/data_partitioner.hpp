6f2a1139edebb5bdc32dbcc36b42a00a1f9abbc3
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-12-12 15:15:22 +0100

    Undo extra substraction

--- implementation/service_authentication/include/data_partitioner.hpp
+++ implementation/service_authentication/include/data_partitioner.hpp
@@ -30,7 +30,7 @@ template <typename T> void data_partitioner::partition_data(const std::string& _
     std::string key_name;
     for(size_t data_key_idx = 0; remaining_bytes > 0; data_key_idx++) {
         key_name = _key_name+std::to_string(data_key_idx);
-        size_t allowed_size = maximum_key_value_pair_size-key_name.size()-2; // 1 for equal sign and 1 for 
+        size_t allowed_size = maximum_key_value_pair_size-key_name.size()-1; // 1 for equal sign
         if (remaining_bytes < allowed_size) {allowed_size = remaining_bytes;}
         _configuration_option->add_item(key_name, std::string(_data.begin()+data_partition_pos,_data.begin()+(allowed_size+data_partition_pos)));
         remaining_bytes = remaining_bytes - allowed_size;
