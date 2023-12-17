515a13646fe7306ec8d4c7c295a478746d47c550
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-12-17 14:36:07 +0100

    Add my subscriber example

---
+++ examples/my-subscriber.cpp
@@ -0,0 +1,155 @@
+// Copyright (C) 2014-2017 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#ifndef VSOMEIP_ENABLE_SIGNAL_HANDLING
+#include <csignal>
+#endif
+#include <chrono>
+#include <condition_variable>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <thread>
+
+#include <vsomeip/vsomeip.hpp>
+
+#include "sample-ids.hpp"
+
+class my_subscriber_app {
+public:
+    my_subscriber_app(bool _use_tcp) :
+            app_(vsomeip::runtime::get()->create_application()), use_tcp_(
+                    _use_tcp) {
+    }
+
+    bool init() {
+        if (!app_->init()) {
+            std::cerr << "Couldn't initialize application" << std::endl;
+            return false;
+        }
+        std::cout << "Client settings [protocol="
+                << (use_tcp_ ? "TCP" : "UDP")
+                << "]"
+                << std::endl;
+
+        app_->register_state_handler(
+                std::bind(&my_subscriber_app::on_state, this,
+                        std::placeholders::_1));
+
+        app_->register_message_handler(
+                vsomeip::ANY_SERVICE, SAMPLE_INSTANCE_ID, vsomeip::ANY_METHOD,
+                std::bind(&my_subscriber_app::on_message, this,
+                        std::placeholders::_1));
+
+        app_->register_availability_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID,
+                std::bind(&my_subscriber_app::on_availability,
+                          this,
+                          std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+
+        std::set<vsomeip::eventgroup_t> its_groups;
+        its_groups.insert(SAMPLE_EVENTGROUP_ID);
+        app_->request_event(
+                SAMPLE_SERVICE_ID,
+                SAMPLE_INSTANCE_ID,
+                SAMPLE_EVENT_ID,
+                its_groups,
+                vsomeip::event_type_e::ET_FIELD);
+        app_->subscribe(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_EVENTGROUP_ID);
+
+        return true;
+    }
+
+    void start() {
+        app_->start();
+    }
+
+#ifndef VSOMEIP_ENABLE_SIGNAL_HANDLING
+    /*
+     * Handle signal to shutdown
+     */
+    void stop() {
+        app_->clear_all_handler();
+        app_->unsubscribe(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_EVENTGROUP_ID);
+        app_->release_event(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_EVENT_ID);
+        app_->release_service(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID);
+        app_->stop();
+    }
+#endif
+
+    void on_state(vsomeip::state_type_e _state) {
+        if (_state == vsomeip::state_type_e::ST_REGISTERED) {
+            app_->request_service(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID);
+        }
+    }
+
+    void on_availability(vsomeip::service_t _service, vsomeip::instance_t _instance, bool _is_available) {
+        std::cout << "Service ["
+                << std::setw(4) << std::setfill('0') << std::hex << _service << "." << _instance
+                << "] is "
+                << (_is_available ? "available." : "NOT available.")
+                << std::endl;
+    }
+
+    void on_message(const std::shared_ptr<vsomeip::message> &_response) {
+        std::stringstream its_message;
+        its_message << "Received a notification for Event ["
+                << std::setfill('0') << std::hex
+                << std::setw(4) << _response->get_service() << "."
+                << std::setw(4) << _response->get_instance() << "."
+                << std::setw(4) << _response->get_method() << "] to Client/Session ["
+                << std::setw(4) << _response->get_client() << "/"
+                << std::setw(4) << _response->get_session()
+                << "] = ";
+        std::shared_ptr<vsomeip::payload> its_payload =
+                _response->get_payload();
+        its_message << "(" << std::dec << its_payload->get_length() << ") "
+		    << std::hex << std::setw(2);
+        for (uint32_t i = 0; i < its_payload->get_length(); ++i)
+            its_message << std::setw(2) << (int) its_payload->get_data()[i] << " ";
+        std::cout << its_message.str() << std::endl;
+    }
+
+private:
+    std::shared_ptr< vsomeip::application > app_;
+    bool use_tcp_;
+};
+
+#ifndef VSOMEIP_ENABLE_SIGNAL_HANDLING
+    my_subscriber_app *subscriber_app_ptr(nullptr);
+    void handle_signal(int _signal) {
+        if (subscriber_app_ptr != nullptr &&
+                (_signal == SIGINT || _signal == SIGTERM))
+            subscriber_app_ptr->stop();
+    }
+#endif
+
+int main(int argc, char **argv) {
+    bool use_tcp = false;
+
+    std::string tcp_enable("--tcp");
+    std::string udp_enable("--udp");
+
+    int i = 1;
+    while (i < argc) {
+        if (tcp_enable == argv[i]) {
+            use_tcp = true;
+        } else if (udp_enable == argv[i]) {
+            use_tcp = false;
+        }
+        i++;
+    }
+
+    my_subscriber_app subscriber_app(use_tcp);
+#ifndef VSOMEIP_ENABLE_SIGNAL_HANDLING
+    subscriber_app_ptr = &subscriber_app;
+    signal(SIGINT, handle_signal);
+    signal(SIGTERM, handle_signal);
+#endif
+    if (subscriber_app.init()) {
+        subscriber_app.start();
+        return 0;
+    } else {
+        return 1;
+    }
+}
