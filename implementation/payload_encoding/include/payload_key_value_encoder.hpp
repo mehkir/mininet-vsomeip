7a26291d622030b91f68b71a321078399965eedf
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-12-18 10:47:42 +0100

    Put in map before encoding

--- implementation/payload_encoding/include/payload_key_value_encoder.hpp
+++ implementation/payload_encoding/include/payload_key_value_encoder.hpp
@@ -2,13 +2,16 @@
 #define VSOMEIP_PAYLOAD_KEY_VALUE_ENCODER_HPP
 
 #include <string>
+#include <map>
 
 class payload_key_value_encoder {
     public:
         payload_key_value_encoder();
         ~payload_key_value_encoder();
-        void add_item(const std::string& _key_name, const std::string& _value, std::string& _payload_data) const ;
+        void add_item(const std::string& _key_name, const std::string& _value) const;
+        std::string encode() const;
     private:
+        mutable std::map<std::string, std::string> plain_key_value_map_;
 };
 
 #endif /* VSOMEIP_PAYLOAD_KEY_VALUE_ENCODER_HPP */
--- implementation/payload_encoding/src/payload_key_value_encoder.cpp
+++ implementation/payload_encoding/src/payload_key_value_encoder.cpp
@@ -8,7 +8,7 @@ payload_key_value_encoder::~payload_key_value_encoder() {
 
 }
 
-void payload_key_value_encoder::add_item(const std::string& _key_name, const std::string& _value, std::string& _payload_data) const {
+void payload_key_value_encoder::add_item(const std::string& _key_name, const std::string& _value) const {
     size_t remaining_bytes = _value.size();
     size_t data_partition_pos = 0;
     int maximum_key_value_pair_size = 0xFF; // DNS-Based SD (rfc6763) maximum of 255 bit of text data after 1 byte length field
@@ -18,12 +18,23 @@ void payload_key_value_encoder::add_item(const std::string& _key_name, const std
         size_t allowed_size = maximum_key_value_pair_size-key_name.size()-1; // 1 for equal sign
         if (remaining_bytes < allowed_size) {allowed_size = remaining_bytes;}
         std::string partioned_data(_value.begin()+data_partition_pos,_value.begin()+(allowed_size+data_partition_pos));
-        uint8_t item_length = (uint8_t) (key_name.size()+1+partioned_data.size()); // 1 for equal sign
-        _payload_data.append(reinterpret_cast<const char*>(&item_length), sizeof(item_length));
-        _payload_data.append(key_name);
-        _payload_data.append("=");
-        _payload_data.append(partioned_data);
+        plain_key_value_map_[key_name] = partioned_data;
         remaining_bytes = remaining_bytes - allowed_size;
         data_partition_pos = data_partition_pos + allowed_size;
     }
+}
+
+std::string payload_key_value_encoder::encode() const {
+    std::string encoded_payload_str;
+    for (auto key_value_pair : plain_key_value_map_) {
+        std::string key_name = key_value_pair.first;
+        std::string value = key_value_pair.second;
+        uint8_t item_length = (uint8_t) (key_name.size()+1+value.size()); // 1 for equal sign
+        encoded_payload_str.append(reinterpret_cast<const char*>(&item_length), sizeof(item_length));
+        encoded_payload_str.append(key_name);
+        encoded_payload_str.append("=");
+        encoded_payload_str.append(value);
+    }
+    plain_key_value_map_.clear();
+    return encoded_payload_str;
 }
