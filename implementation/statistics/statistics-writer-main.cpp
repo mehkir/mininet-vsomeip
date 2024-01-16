0b8d65270d2c38267afffc87349d8c464a6ecd84
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2024-01-16 19:58:56 +0100

    Add statistics writer main code

--- implementation/statistics/statistics-writer-main.cpp
+++ implementation/statistics/statistics-writer-main.cpp
@@ -1,6 +1,28 @@
+#include "include/statistics_writer.hpp"
 #include <iostream>
+#include <memory>
 
 int main (int argc, char* argv[]) {
-    std::cout << argc << ", " << argv[0] << std::endl;
-    std::cout << "Hello World!" << std::endl;
+    if(argc != 3) {
+      std::cerr << "Usage: " + std::string(argv[0]) + " <member_count> <absolute_results_directory_path>\n";
+      std::cerr << "  Example: " + std::string(argv[0]) + " 20 /path/to/results/directory\n";
+      return 1;
+    }
+    std::uint32_t member_count = std::stoi(argv[1]);
+    std::string absolute_results_directory_path(argv[2]);
+    std::string result_filename;
+    result_filename = "mininet-vsomeip";
+    if (member_count <= 1) {
+      std::cerr << "member_count must be greater than 1\n";
+      return 1;
+    }
+    result_filename += "-" + std::to_string(member_count);
+
+    std::string slash_char("/");
+    if (absolute_results_directory_path.compare(absolute_results_directory_path.length()-1,1,slash_char)) {
+        absolute_results_directory_path += "/";
+    }
+    std::unique_ptr<statistics_writer> sw(statistics_writer::get_instance(member_count, absolute_results_directory_path, result_filename));
+    sw->write_statistics();
+    return 0;
 }
